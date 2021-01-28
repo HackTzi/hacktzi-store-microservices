8cf78ead9bb28c6179e7bf6c3a56c1f358a5dc13
Dante Calderon <mail@dantecalderon.dev>
2021-01-27 23:23:28 -0500

    Add RUD operations

--- swag/platzi-swag-posts/src/posts/posts.controller.ts
+++ swag/platzi-swag-posts/src/posts/posts.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { UpdatePostDto } from './dtos/update-post.dto';
 import { PostsService } from './posts.service';
 
 @ApiTags('Posts')
@@ -11,5 +22,26 @@ export class PostsController {
   @Post()
   async create(@Body() data: CreatePostDto) {
     return this.postsService.create(data);
+  }
+
+  @Get()
+  async find() {
+    return this.postsService.find();
+  }
+
+  @Get('/:id')
+  async findById(@Param('id') id: string) {
+    return this.postsService.findById(id);
+  }
+
+  @Patch('/:id')
+  async update(@Param('id') id: string, @Body() data: UpdatePostDto) {
+    return this.postsService.update(id, data);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string) {
+    return this.postsService.deleteById(id);
   }
 }
