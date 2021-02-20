1d33a55cf7f844720c1b451ecb25e63ce0d79513
yaderson <yadersonjesuspatinovidal@gmail.com>
2021-02-20 13:24:05 -0500

    Fix: migrations in docker

--- products/hacktzi_store_products/src/app.module.ts
+++ products/hacktzi_store_products/src/app.module.ts
@@ -5,29 +5,20 @@ import { DepartmentModule } from './department/department.module';
 import { ProductModule } from './product/product.module';
 import { ProductSubscriber } from './product/product.subscriber';
 
-/** env */
-const { 
-  DB_NAME,
-  DB_USER, 
-  DB_PASSWORD,
-  NODE_ENV, 
-  DB_PORT, 
-  DB_HOST
-} = process.env
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: DB_HOST,
-      port: parseInt(DB_PORT),
-      username: DB_USER,
-      password: DB_PASSWORD,
-      database: DB_NAME,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: ['dist/**/*.entity{.ts,.js}'],
       subscribers: [ProductSubscriber],
-      synchronize: NODE_ENV == 'production'|| false, //TODO: Db schema will be auto created on every application launch. AVOID LATER!!
+      synchronize: false,
+      //synchronize: process.env.NODE_ENV == 'production'|| false, //TODO: Db schema will be auto created on every application launch. AVOID LATER!!
       retryDelay: 6000,
       retryAttempts: 10
     }),
